e8887eeffdb853c428e4b4d2312558c23da72822
Tauki Tahmid <ttauki@gmail.com>
2018-01-13 04:41:05 +0600

    Add solutions for assignment 1

---
+++ assignments/assignment 1/Task 2/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Insert a number");
+        int n = Integer.parseInt(keyboard.nextLine());
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i+1; j++) System.out.print(j+1 + " ");
+            System.out.println();
+        }
+    }
+}
