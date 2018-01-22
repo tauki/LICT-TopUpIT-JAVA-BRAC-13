6dcfb2acef019a60119b336d5e0792043f46baa0
Tauki Tahmid <ttauki@gmail.com>
2018-01-22 23:26:36 +0600

    Add loginForm and actionPerformer

--- assignments/assignment 10/Form.java
+++ assignments/assignment 10/Form.java
@@ -7,24 +7,45 @@ class Form extends JFrame implements ActionListener {
     JTextField uname_txt, email_txt;
     JPasswordField pwd_txt;
     JButton register, login;
+    String registerLabel = "Register",
+            loginLabel = "Login",
+            registerLabelAlt = "Take me to sign up",
+            loginLabelAlt = "Take me to sign in",
+            usernameLabel = "Username",
+            passwordLabel = "Password",
+            emailLabel = "Email";
 
     public Form(String title){
         super(title);
         setLayout(new FlowLayout());
-        JPanel panel = new JPanel(
-                new GridLayout(4, 2, 10, 20)
+
+        if(title.equals(reg))
+            initiateRegisterForm(
+                    new JPanel(
+                            new GridLayout(4, 2, 10, 20)
+                    )
+            );
+
+        else initiateLoginForm(
+                new JPanel(
+                        new GridLayout(3, 2, 10, 20)
+                )
         );
 
-        uname = new JLabel("User Name");
-        pwd = new JLabel("Password");
-        email = new JLabel("Email");
+        setDefaultCloseOperation(this.Exit_ON_CLOSE);
+    }
+
+    public void initiateRegisterForm(JPanel panel) {
+        uname = new JLabel(usernameLabel);
+        pwd = new JLabel(passwordLabel);
+        email = new JLabel(emailLabel);
 
         uname_txt = new JTextField(10);
         pwd_txt = new JPasswordField(20);
         email_txt = new JTextField(30);
 
-        register = new JButton("Register");
-        login = new JButton("Take me to login");
+        register = new JButton(registerLabel);
+        login = new JButton(loginLabelAltAlt);
 
         panel.add(uname);
         panel.add(uname_txt);
@@ -38,8 +59,51 @@ class Form extends JFrame implements ActionListener {
         add(panel);
         register.addActionListener(this);
         login.addActionListener(this);
+    }
 
-        setDefaultCloseOperation(this.Exit_ON_CLOSE);
+    public void initiateLoginForm(JPanel panel) {
+        // currently accepting only email and pass
+        // uname = new JLabel("User Name");
+        pwd = new JLabel(passwordLabel);
+        email = new JLabel(emailLabel);
+
+        //uname_txt = new JTextField(10);
+        pwd_txt = new JPasswordField(20);
+        email_txt = new JTextField(30);
+
+        register = new JButton(registerLabelAltAlt);
+        login = new JButton(loginLabel);
+
+        //panel.add(uname);
+        //panel.add(uname_txt);
+        panel.add(email);
+        panel.add(email_txt);
+        panel.add(pwd);
+        panel.add(pwd_txt);
+        panel.add(register);
+        panel.add(login);
+
+        add(panel);
+        register.addActionListener(this);
+        login.addActionListener(this);
+    }
+
+    public void showSuccess() {
+        JOptionPane.showMessageDialog(
+                this,
+                "Successful",
+                "Smile..."
+                JOptionPane.INFORMATION_MESSAGE
+        );
+    }
+
+    public void showError() {
+        JOptionPane.showMessageDialog(
+                this,
+                Validator.Errors,
+                "Sorry...",
+                JOptionPane.ERROR_MESSAGE
+        );
     }
 
     public void actionPerformed(ActionEvent ae) {
@@ -54,20 +118,19 @@ class Form extends JFrame implements ActionListener {
                     && !Validator.emailExists(email)
                     && Validator.validatePassword(pwd)
                     )
-                JOptionPane.showMessageDialog(
-                        this,
-                        "Successful",
-                        "Smile..."
-                        JOptionPane.INFORMATION_MESSAGE
-                );
-            else JOptionPane.showMessageDialog(
-                    this,
-                    Validator.Errors,
-                    "Sorry...",
-                    JOptionPane.ERROR_MESSAGE
-            );
+                showSuccess();
+            else showError();
+
+        } else if(action.equals(login.getLabel())) {
+            if(Validator.verifyUserCred(email, pwd))
+                showSuccess();
+            else showError();
 
         } else {
+            new Form(
+                    action.equals(registerLabelAlt) ? registerLabel : loginLabel
+            ).setVisible(true);
+            this.dispose();
 
         }
     }
